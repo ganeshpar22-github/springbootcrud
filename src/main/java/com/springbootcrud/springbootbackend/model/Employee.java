c18198df783b8fa50a94a15967620ede5fac9452
Ganesh Parmar <ganeshparmar221997@gmail.com>
2022-04-21 19:01:33 +0530

    [SpringbootCrud App] Added JPA Entity and EmployeeRepository

---
+++ src/main/java/com/springbootcrud/springbootbackend/model/Employee.java
@@ -0,0 +1,89 @@
+package com.springbootcrud.springbootbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="first_name", nullable=false)
+	private String firstName;
+	
+	@Column(name="last_name", nullable=false)
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="address")
+	private String address;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Employee(long id, String firstName, String lastName, String email, String address) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+	}
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", address=" + address + "]";
+	}
+	
+	
+	
+	
+
+}
