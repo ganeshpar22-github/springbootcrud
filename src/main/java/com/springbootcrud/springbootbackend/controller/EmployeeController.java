46b4ecb5af14bca4f0f833d1c12d1692c26b85ae
Ganesh Parmar <ganeshparmar221997@gmail.com>
2022-04-22 21:58:58 +0530

    [SpringBootCrud App] Added CreateEmployee and getAllEmployee REST API's

---
+++ src/main/java/com/springbootcrud/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.springbootcrud.springbootbackend.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootcrud.springbootbackend.model.Employee;
+import com.springbootcrud.springbootbackend.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")    //for generating base url so no need to write below on post mapping
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	//REST API
+	
+	//create employee rest api
+	
+	@PostMapping
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+	
+	//get all employees
+	
+	@GetMapping
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+	
+}
---
+++ src/main/java/com/springbootcrud/springbootbackend/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.springbootcrud.springbootbackend.service;
+
+import java.util.List;
+
+import com.springbootcrud.springbootbackend.model.Employee;
+
+public interface EmployeeService {
+	
+	Employee saveEmployee(Employee employee);
+	
+	List<Employee> getAllEmployees();
+
+}
